fddc12621b5605efd87adf3a5d6beb11acc9ec7e
adolfoartaza <adolfoartaza@gmail.com>
2015-04-20 18:50:50 -0400

    Everything works properly
    Also, separated project into different folders and files

---
+++ public/js/controllers/main.controller.js
@@ -0,0 +1,36 @@
+app.controller('MainController', function ($scope, FlashCardsFactory, ScoreFactory) {
+	$scope.getAllCards = function() {
+		$scope.currentCategory = null;
+		
+		// FlashCardsFactory.get... returns a promise, that has to be translated into action using .then method
+		FlashCardsFactory.getFlashCards().then(function (cards) {
+			$scope.flashCards = cards;
+		});
+	};
+
+	// Note that $scope.getAllCards() gets called by the controller itself, to ensure that once the controller
+	// is loaded, the cards get loaded as well.
+	$scope.getAllCards();
+
+	$scope.categories = [ 'MongoDB', 'Express', 'Angular', 'Node' ];
+
+	
+	$scope.getCategoryCards = function (category) {
+		FlashCardsFactory.getFlashCards(category).then(function (cards) {
+			$scope.flashCards = cards;
+			$scope.currentCategory = category;
+		});
+	}
+	
+	$scope.answerQuestion = function (answer, flashCard) {
+		if (!flashCard.answered) {
+			flashCard.answered = true;
+			flashCard.answeredCorrectly = answer.correct;
+			if(flashCard.answeredCorrectly)
+				ScoreFactory.correct++;
+			else
+				ScoreFactory.incorrect++;
+		}
+	}
+
+});
